523d6eab24fbf9f5177c92c1c1e8311a7263c700
Lucilef81 <lucile.franzinetti@gmail.com>
2020-02-21 22:29:24 +0100

    pn  enfin dans sa case + github centré

--- src/components/Header/HeaderStyled.js
+++ src/components/Header/HeaderStyled.js
@@ -4,6 +4,8 @@ const HeaderStyled = styled.div`
   .header-icon {
     display: flex;
     align-items: center;
+    justify-content: center;
+    margin-bottom: 1rem;
 
     h1 {
       margin-top: 0;
--- src/components/RepoResults/ReposStyled.js
+++ src/components/RepoResults/ReposStyled.js
@@ -3,9 +3,14 @@ import styled from 'styled-components';
 const ReposStyled = styled.div`
   .ui.grid {
     margin: 0 auto;
+    width: 60%;
   }
   .ui.grid > .row > .column {
     margin-bottom: 1rem;
+
+    .meta + .description {
+      word-wrap: break-word;
+    }
   }
 
   .ui.card > .content {
